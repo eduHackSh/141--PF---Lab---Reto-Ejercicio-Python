538f32e06de1a5ea7379ba8734326d6c856413d0
Eduardo.sh <114368042+eduHackSh@users.noreply.github.com>
2022-10-31 21:37:54 -0300

    Creando el archivo!

---
+++ primos.py
@@ -0,0 +1,22 @@
+import os
+
+r = 250
+suma = float()
+#formula: (n-1)!+1 es multiplo de n
+txt = ""
+
+for x in range(1, r+1):
+    for num in range(1, (x-1)):
+        if suma == 0:
+            suma += ((x-1)*(x-1-num))
+        else:
+            suma = suma*(x-1-num)
+    
+
+
+    if ((suma+1)%x == 0 or x==2):
+            txt += f"{x}\n"
+    suma = 0
+
+file = open('resultado.txt', 'w')
+file.write(txt)
